e82ccee2d77ef3e7748bae04e2b9ddcd76e456b3
BohdanLytvynov <blitvinov19@gmail.com>
2026-02-14 11:37:07 +0200

    add interactivity

--- LabWork3/Results/WordCountResult.cs
+++ LabWork3/Results/WordCountResult.cs
@@ -4,6 +4,8 @@ namespace LabWork3.Results
 {
     internal class WordCountResult : ConsumerResult<int>
     {
+        public string FileName { get; set; }
+
         public WordCountResult(int result, Exception exception) : base(result, exception)
         {
         }
